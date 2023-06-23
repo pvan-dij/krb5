27a6ad1a06954175bbbc8550719fd8ce9a6dd1bd
pvan-dij <86962846+pvan-dij@users.noreply.github.com>
2023-06-23 11:00:44 +0200

    we ballin

--- src/plugins/kdb/ldap/libkdb_ldap/kdb_ldap_conn.c
+++ src/plugins/kdb/ldap/libkdb_ldap/kdb_ldap_conn.c
@@ -58,6 +58,8 @@ validate_context(krb5_context context, krb5_ldap_context *ctx)
         return 0;
     }
 
+    printf("bruhDGASFSFASFDSFASFSFAAADKFLSFJKSJFIWOFNWOIENFWN\n");
+
     /* For a simple bind, a DN and password are required. */
 
     if (ctx->bind_dn == NULL) {
